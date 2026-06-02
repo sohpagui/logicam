f7b71271f7302f9cd2af67d7bb56b0a6bbb0a522
sohpagui <paguipascal6@gmail.com>
2026-06-02 21:58:18 +0100

    correction debordement horizontal mobile toutes les pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
     <html lang="fr">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AuthProvider>
-          {children}
+          <div className="max-w-[100vw] overflow-x-hidden">
+            {children}
+          </div>
         </AuthProvider>
       </body>
     </html>
